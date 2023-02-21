497e3dd1f387f53ca904846f09ca70be445483ac
3Dlyra <3dlyra.com@gmail.com>
2023-02-22 01:18:44 +0500

    Implement `intersectsPoint` method

--- line3.d.ts
+++ line3.d.ts
@@ -1,1 +1,2 @@
 export function create( v3a: Float32Array, v3b: Float32Array ): Float32Array;
+export function intersectsPoint( line: Float32Array, v3: Float32Array, tolerance?: boolean ): boolean;
--- line3.js
+++ line3.js
@@ -2,3 +2,14 @@ export function create( v3a, v3b ) {
 
 	return new Float32Array( [ ...v3a, ...v3b ] )
 }
+
+export function intersectsPoint( line, v3, tolerance ) {
+
+	tolerance = tolerance || 0
+
+	const AB = Math.sqrt( ( line[ 0 ] - line[ 3 ] ) ** 2 + ( line[ 1 ] - line[ 4 ] ) ** 2 + ( line[ 2 ] - line[ 5 ] ) ** 2 )
+	const AP = Math.sqrt( ( line[ 0 ] - v3[ 0 ] ) ** 2 + ( line[ 1 ] - v3[ 1 ] ) ** 2 + ( line[ 2 ] - v3[ 2 ] ) ** 2 )
+	const BP = Math.sqrt( ( line[ 3 ] - v3[ 0 ] ) ** 2 + ( line[ 4 ] - v3[ 1 ] ) ** 2 + ( line[ 5 ] - v3[ 2 ] ) ** 2 )
+
+	return Math.abs( AB - ( AP + BP ) ) <= tolerance
+}
