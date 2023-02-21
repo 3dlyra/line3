e795cdd6df2e9fb2363e984851e5bb5cbed37934
3Dlyra <3dlyra.com@gmail.com>
2023-02-22 02:33:55 +0500

    Fix misspelling

--- line3.js
+++ line3.js
@@ -7,9 +7,9 @@ export function intersectsPoint( line3, v3, tolerance ) {
 
 	tolerance = tolerance || 0
 
-	const AB = Math.sqrt( ( line[ 0 ] - line[ 3 ] ) ** 2 + ( line[ 1 ] - line[ 4 ] ) ** 2 + ( line[ 2 ] - line[ 5 ] ) ** 2 )
-	const AP = Math.sqrt( ( line[ 0 ] - v3[ 0 ] ) ** 2 + ( line[ 1 ] - v3[ 1 ] ) ** 2 + ( line[ 2 ] - v3[ 2 ] ) ** 2 )
-	const BP = Math.sqrt( ( line[ 3 ] - v3[ 0 ] ) ** 2 + ( line[ 4 ] - v3[ 1 ] ) ** 2 + ( line[ 5 ] - v3[ 2 ] ) ** 2 )
+	const AB = Math.sqrt( ( line3[ 0 ] - line3[ 3 ] ) ** 2 + ( line3[ 1 ] - line3[ 4 ] ) ** 2 + ( line3[ 2 ] - line3[ 5 ] ) ** 2 )
+	const AP = Math.sqrt( ( line3[ 0 ] - v3[ 0 ] ) ** 2 + ( line3[ 1 ] - v3[ 1 ] ) ** 2 + ( line3[ 2 ] - v3[ 2 ] ) ** 2 )
+	const BP = Math.sqrt( ( line3[ 3 ] - v3[ 0 ] ) ** 2 + ( line3[ 4 ] - v3[ 1 ] ) ** 2 + ( line3[ 5 ] - v3[ 2 ] ) ** 2 )
 
 	return Math.abs( AB - ( AP + BP ) ) <= tolerance
 }
