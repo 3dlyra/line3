63da4ba8c5abb66b748e15e7b02e35ad071d38c1
3Dlyra <3dlyra.com@gmail.com>
2023-02-22 01:02:38 +0500

    Implement `create` method

---
+++ line3.d.ts
@@ -0,0 +1,1 @@
+export function create( v3a: Float32Array, v3b: Float32Array ): Float32Array;
---
+++ line3.js
@@ -0,0 +1,4 @@
+export function create( v3a, v3b ) {
+
+	return new Float32Array( [ ...v3a, ...v3b ] )
+}
