42e57b7c198ad1ed5bb54014a95b7fc9dca38585
3Dlyra <3dlyra.com@gmail.com>
2023-02-22 01:58:46 +0500

    Implement `nearestPoint` method

--- line3.js
+++ line3.js
@@ -13,3 +13,44 @@ export function intersectsPoint( line3, v3, tolerance ) {
 
 	return Math.abs( AB - ( AP + BP ) ) <= tolerance
 }
+
+export function nearestPoint( line3, v3 ) {
+
+	const AP = ( line3[ 0 ] - v3[ 0 ] ) ** 2 + ( line3[ 1 ] - v3[ 1 ] ) ** 2 + ( line3[ 2 ] - v3[ 2 ] ) ** 2
+	const BP = ( line3[ 3 ] - v3[ 0 ] ) ** 2 + ( line3[ 4 ] - v3[ 1 ] ) ** 2 + ( line3[ 5 ] - v3[ 2 ] ) ** 2
+
+	const aSubB = [ line3[ 0 ] - line3[ 3 ], line3[ 1 ] - line3[ 4 ], line3[ 2 ] - line3[ 5 ] ]
+	const bSubA = [ line3[ 3 ] - line3[ 0 ], line3[ 4 ] - line3[ 1 ], line3[ 5 ] - line3[ 2 ] ]
+	const pSubA = [ v3[ 0 ] - line3[ 0 ], v3[ 1 ] - line3[ 1 ], v3[ 2 ] - line3[ 2 ] ]
+
+	const AB = bSubA[ 0 ] ** 2 + bSubA[ 1 ] ** 2 + bSubA[ 2 ] ** 2
+	const dot = pSubA[ 0 ] * bSubA[ 0 ] + pSubA[ 1 ] * bSubA[ 1 ] + pSubA[ 2 ] * bSubA[ 2 ]
+
+	const perpendicular = [
+		line3[ 0 ] + bSubA[ 0 ] * dot / AB,
+		line3[ 1 ] + bSubA[ 1 ] * dot / AB,
+		line3[ 2 ] + bSubA[ 2 ] * dot / AB
+	]
+
+	const centerPoint = [
+		line3[ 0 ] - aSubB[ 0 ] / 2,
+		line3[ 1 ] - aSubB[ 1 ] / 2,
+		line3[ 2 ] - aSubB[ 2 ] / 2,
+	]
+
+	const endPoint = AP <= BP ? [ line3[ 0 ], line3[ 1 ], line3[ 2 ] ] : [ line3[ 3 ], line3[ 4 ], line3[ 5 ] ]
+
+	const aDistanceToN = ( line3[ 0 ] - perpendicular[ 0 ] ) ** 2 + ( line3[ 1 ] - perpendicular[ 1 ] ) ** 2 + ( line3[ 2 ] - perpendicular[ 2 ] ) ** 2
+	const bDistanceToN = ( line3[ 3 ] - perpendicular[ 0 ] ) ** 2 + ( line3[ 4 ] - perpendicular[ 1 ] ) ** 2 + ( line3[ 5 ] - perpendicular[ 2 ] ) ** 2
+	const nDistanceToB = ( perpendicular[ 0 ] - line3[ 3 ] ) ** 2 + ( perpendicular[ 1 ] - line3[ 4 ] ) ** 2 + ( perpendicular[ 2 ] - line3[ 5 ] ) ** 2
+
+	const isNegative = Math.sqrt( aDistanceToN ) + Math.sqrt( bDistanceToN ) > Math.sqrt( AB ) && aDistanceToN < bDistanceToN
+	const time = Math.sqrt( aDistanceToN ) / Math.sqrt( AB )
+
+	return new Float32Array( [
+		...endPoint,
+		...perpendicular,
+		...centerPoint,
+		isNegative ? - time : time,
+	] )
+}
